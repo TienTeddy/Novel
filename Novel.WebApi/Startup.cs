302601966cb859d3d9e687da3cab90a197ccad09
TienTeddy <Tienle10998@gmail.com>
2021-08-04 23:03:43 +0700

    Add Swagger API

--- Novel.WebApi/Startup.cs
+++ Novel.WebApi/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Novel.Business.Catalog.Products;
 using Novel.DAL.EF;
 using System;
@@ -34,6 +35,23 @@ namespace Novel.WebApi
             services.AddTransient<IPublicProductService, PublicProductService>();
 
             services.AddControllersWithViews();
+
+            //Swaggger API
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Novel API",
+                    Version = "v1",
+                    Description = "Swagger Api V1 of Novel.",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Tien Lee",
+                        Email = "Tienle10998@gmail.com",
+                        Url =  new Uri("https://www.facebook.com/tienlee47"),
+                    },
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,6 +73,19 @@ namespace Novel.WebApi
             app.UseRouting();
 
             app.UseAuthorization();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger Novel API V1");
+
+                // To serve SwaggerUI at application's root page, set the RoutePrefix property to an empty string.
+                //c.RoutePrefix = string.Empty;
+            });
 
             app.UseEndpoints(endpoints =>
             {
